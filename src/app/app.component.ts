0edb118fcda5df12812670b12d73f0c04e3cc9fa
LabsForAll <oneFor91@gmail.com>
2017-04-05 12:36:23 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var util : any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit(){
+    
+    if (util.supports.data && util.supports.audioVideo) {
+      console.log("browser is ok");
+    }
+  }
 }
